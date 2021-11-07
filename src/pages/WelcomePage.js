aee06d0d306d77e2a38cc698040f3ecfb5a4294b
nolastemgarden <nolastemgarden@gmail.com>
2021-11-07 13:02:56 -0600

    Update WelcomePage.js

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -19,16 +19,6 @@ import AppBar from '@material-ui/core/AppBar'
 // Custom Styling
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
-    // root: {
-    //     // borderRight: 'solid black 1rem',
-    //     width: '100%',
-    //     minHeight: 'calc(100vh - 4rem)',
-
-    //     display: 'flex',
-    //     flexDirection: 'column',
-    //     color: theme.palette.common.white,
-    //     padding: '1.0rem',
-    // }, 
     button: {
         backgroundColor: theme.palette.primary.main,
         textAlign: 'center',
@@ -46,64 +36,43 @@ export default function WelcomePage() {
             </AppBar>
             <Container className={classes.root} maxWidth='md' >
                 <Box p={2} />
-
-                <Typography align='center' color='textPrimary' component='h1' variant='h2' gutterBottom >
-                    Tic-Tac-Toe & The Fifteen Game
-                </Typography>
-                <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
+                <Typography align='center' color='textPrimary' component='h1' variant='h3' gutterBottom >
                     A Math Lesson in Disguise
                 </Typography>
                 <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                    This lesson aims to help students understand two key points about mathematics: 
+                    Mathematical thinking has a playful side. 
+                    In fact, there are whole branches of study known as "game theory" and "recreational mathematics". 
+                    Sadly, this side of mathematics has been excluded from many primary and secondary school curriculums by folks who
+                     are deluded into thinking that we don't have enough time in the school day to teach anything but "essential skills".
+                    The result of this approach is that many people give up on math altogether before they get to "the good part". 
+                    This project seeks to counteract that trend by introducing two of the most important concepts in mathematics through 
+                    two simple strategy games.  One of these games is likely very familiar to you, the other is probably not.  They both have some surprising things to teach us.  
+               </Typography>
+                <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+                    This project seeks to counteract that trend by using two simple strategy games to introduce two of the most important concepts in math.
+                    One of these games is likely familiar to you, the other is probably not. 
+                    They both have a surprising amount to teach, if you play along.
                 </Typography>
                 <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                    1) All Mathematics starts out with a limited set of simple assumptions and builds upon that foundation by putting 
-                    those pieces together in creative ways to prove more complex ideas must be true. As Jordan Ellenberg put it, 
-                    "Mathematics is the extension of of common sense by other means."
-                </Typography>
-                <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                    2) Mathematics strips away the messy "real world" parts of problems and looks for
-                    patterns in the inner workings of things. We can deepen our understanding of what seems to be an unfamiliar
-                    situation by looking for ways that it is similar to a more familiar situation beneath the surface.
-                </Typography>
-
-                <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
-                    Tic-Tac-Toe as an Intro to Proof
-                </Typography>
-                <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                    Proof is the foundation that all Mathematics sits upon, but the practice of writing "proofs" is often
-                    taught in a needlessly confusing way. In American schools, students are typically asked to write their first
-                    proofs in the context of a geometry class, at the same time as they are being introduced to a lot of new
-                    concepts and vocabulary dealing with similar and congruent triangles. This results in people never really 
-                    coming to understand how a proof is different than a plausible explanation or meerly "showing your work". 
-                </Typography>
-                <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                    Proof should be introduced in a context that is simple and familiar. My favorite way to introduce the concept 
-                    of proof makes use of the classic game Tic Tac Toe. I challenge you to write down a detailed stategy that 
-                    one player or the other can use to win every time. If you think no such winning strategy exists, then prove that 
-                    instead, that the best outcome either player can ensure is a draw.
-                </Typography>
-                <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                    This can be done with nothing more than pencil and paper! It will take several pages and it will challenge your 
-                    ability to organize your thoughts. It's going to take more than just playing lots of games and recording the results. 
-                    Even if you win a thousand games in a row, that does not prove you have a winning strategy, just that you have an 
-                    opponent who doesn't learn their lesson. To actually prove you have a winning strategy you will have to examine every 
-                    possibility, not just the ones a particular opponent plays. Making a tree-diagram that maps out all variations will help 
-                    you ensure that you have not overlooked any possible moves. There are shortcuts you can take based on symmetry. You may 
-                    find it helpful to develop your own shorthand notation for a position. You will certainly need to come up with a precice 
-                    definition of the word "mistake".
-                </Typography>
+                    This lesson has something to offer students of all experience levels.
+                    Kids of Elementary School age can use it to practice their fluency at mentally computing sums and differences.
+                    Middle School aged kids will learn about combinations and permutations as they build their tree diagrams and consider all the
+                    possible things that could happen next. 
+                    High School aged kids will learn what it means to write a rigorous proof and can apply their algebra skills to create a Magic Square from scratch.
+               </Typography>
                 <Box width="100%" mb={3} >
-                    <Grid container>
+                    <Grid container >
+                        <Grid item xs={0} sm={2} />
+                            
                         <Grid item xs={12} sm={4} >
                             <Button
                                 className={classes.button}
                                 variant="contained"
                                 color="primary"
                                 component={RouterLink}
-                                to='/play_vs_human'
+                                to='/tic_tac_toe_rules'
                             >
-                                Play Human vs. Human
+                                Tic Tac Toe
                             </Button>
                         </Grid>
                         <Grid item xs={12} sm={4} >
@@ -112,25 +81,42 @@ export default function WelcomePage() {
                                 variant="contained"
                                 color="primary"
                                 component={RouterLink}
-                                to='/play_vs_bot'
+                                to='/fifteen_rules'
                             >
-                                Play Human vs. Bot
+                                The Fifteen Game
                             </Button>
                         </Grid>
-                        <Grid item xs={12} sm={4} >
-                            <Button
-                                className={classes.button}
-                                variant="contained"
-                                color="primary"
-                                component={RouterLink}
-                                to='/play_with_coach'
-                            >
-                                Play with Coach
-                            </Button>
-                        </Grid>
+                        <Grid item xs={0} sm={2} />
+
                     </Grid>
                 </Box>
-                <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
+
+                {/* <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
+                    Tic-Tac-Toe: An Introduction to Proof Writing
+                </Typography>
+                <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+                    By the time they have played Tic-Tac-Toe a few dozen times most children have gotten pretty good at the game. 
+                    At this point I like to challenge them to find (and write down) a strategy that will guarantee them a win 
+                    every time, as long as they get to make the first move. 
+                    After finding counterexamples to their first few attempts, most kids will intuit that this is an impossible task. 
+                    At this point I modify the challenge: If you think there is no winning strategy, then prove that instead!
+                    Prove that you can at least get a draw no matter what moves your opponent makes when they go first.
+                    The point here is that we will not settle for a final answer that is just based on intuition.
+                    This task can be completed with simple tools, nothing more than pencil and paper is required! 
+                    It will take several pages and it will challenge your ability to be thorough and organized in your thinking.
+                </Typography>
+                <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+                    Finding an actual proof is going to take more than just playing lots of games and recording the results. 
+                    Even if you win a thousand games in a row, that does not prove you have a winning strategy, just that you have an 
+                    opponent who's not learning from their mistakes. To actually prove you have a winning strategy you will have to examine every 
+                    possibile play, not just the ones a particular opponent chooses. Making a tree-diagram that maps out all variations will help 
+                    you ensure that you have not overlooked any possible moves. There are shortcuts you can take based on symmetry. You may 
+                    find it helpful to develop your own shorthand notation for a position. You will certainly need to come up with a precice 
+                    definition of the word "mistake".  Exploring possibilities, finding counterexamples, developing new forms of notation, 
+                    and formulating definitions... This IS what doing Math involves.
+                </Typography> */}
+                
+                {/* <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
                     The Fifteen Game: Similarity Beneath the Surface
                 </Typography>
                 <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
@@ -141,7 +127,7 @@ export default function WelcomePage() {
                     Experience this for yourself by playing the two game featured here. 
                     Though the two games feel nothing alike when you first play them, the Fifteen Game is startegically identical to Tic-Tac-Toe. 
                     If you have mastered Tic-Tac-Toe but still find yourself losing to my bot ath the Fifteen Game then I suggest looking into "Magic Squares".
-                </Typography>
+                </Typography> */}
 
 
                 <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
@@ -194,3 +180,53 @@ export default function WelcomePage() {
 //     Is it possible for the player who goes first to make a mistake on the first move of the game? 
 //     Is is possible for the player who goes second to make a mistake on their first move?
 // </Typography> 
+
+
+{/* <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+                    1) All Mathematics starts out with a limited set of simple assumptions and builds upon that foundation by putting 
+                    those pieces together in creative ways to prove more complex ideas must be true. As Jordan Ellenberg put it, 
+                    "Mathematics is the extension of of common sense by other means."
+                </Typography>
+                <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+                    2) Mathematics strips away the messy "real world" parts of problems and looks for
+                    patterns in the inner workings of things. We can deepen our understanding of what seems to be an unfamiliar
+                    situation by looking for ways that it is similar to a more familiar situation beneath the surface.
+                </Typography> */}
+
+{/* <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+    At its core, Mathematics is about looking for patterns and trying to explain with logic why those patterns must be true.  
+</Typography>
+
+
+<Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+    In math class, the art of writing proofs is often introduced in a needlessly confusing way. 
+    It is usually mixed into a lesson that also introduces lots of new technical jargon deaing with similar and congruent triangles. 
+    I think that kids schould be introduced to the art of writing really thorough and persuasive arguments in a context that is more familiar. 
+</Typography> */}
+
+
+
+
+// <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+//     Proof is the foundation that all Mathematics sits upon, but the practice of writing "proofs" is often
+//     taught in a needlessly confusing way. In American schools, students are typically asked to write their first
+//     proofs in the context of a geometry class, at the same time as they are being introduced to a lot of new
+//     concepts and vocabulary dealing with similar and congruent triangles. This results in people never really 
+//     coming to understand how a proof is different than a plausible explanation or meerly "showing your work". 
+// </Typography>
+// <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+//     Proof should be introduced in a context that is simple and familiar. My favorite way to introduce the concept 
+//     of proof makes use of the classic game Tic Tac Toe. I challenge you to write down a detailed stategy that 
+//     one player or the other can use to win every time. If you think no such winning strategy exists, then prove that 
+//     instead, that the best outcome either player can ensure is a draw.
+// </Typography>
+// <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+//     This can be done with nothing more than pencil and paper! It will take several pages and it will challenge your 
+//     ability to organize your thoughts. It's going to take more than just playing lots of games and recording the results. 
+//     Even if you win a thousand games in a row, that does not prove you have a winning strategy, just that you have an 
+//     opponent who doesn't learn their lesson. To actually prove you have a winning strategy you will have to examine every 
+//     possibility, not just the ones a particular opponent plays. Making a tree-diagram that maps out all variations will help 
+//     you ensure that you have not overlooked any possible moves. There are shortcuts you can take based on symmetry. You may 
+//     find it helpful to develop your own shorthand notation for a position. You will certainly need to come up with a precice 
+//     definition of the word "mistake".
+// </Typography>
