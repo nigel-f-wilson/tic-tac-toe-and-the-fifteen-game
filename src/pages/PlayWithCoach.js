540e889a6791369dd0f24018a9d6a369c34fbcb3
nolastemgarden <nolastemgarden@gmail.com>
2021-11-07 13:02:48 -0600

    Update PlayWithCoach.js

--- src/pages/PlayWithCoach.js
+++ src/pages/PlayWithCoach.js
@@ -18,7 +18,8 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     root: {
         width: '100%',
-        height: '95%',
+        height: '100vh',
+        overflowY: 'hidden',
         display: 'flex',
         flexDirection: 'column',
     },
