9de4e278abe3ba697c23e901dd8cec504e814b5a
nolastemgarden <nolastemgarden@gmail.com>
2022-01-09 14:07:30 -0600

    Update FifteenRules.js

--- src/pages/FifteenRules.js
+++ src/pages/FifteenRules.js
@@ -14,31 +14,19 @@ import {
     Grid, 
     Button, 
     Typography,
-    Card, 
-    CardActionArea, 
-    CardMedia, 
-    CardHeader } from '@material-ui/core'
-
-// Images
-import botEmoji from "../images/robot_emoji.png";
-import childEmoji from "../images/child_emoji.png";
-import teacherEmoji from "../images/teacher_emoji.png";
+} from '@material-ui/core'
+import Icon from '@material-ui/core/Icon';
 
 // Custom Styling
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     scrollingContainer: {
-        overflowY: "scroll"
-    },
-    cardRoot: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#fff"
-    },
-    cardTitle: {
-        color: "#000",
+        overflowY: "scroll",
+        "scrollbarWidth": "none",       /* Firefox */
+        "MsOverflowStyle": "none",      /* IE and Edge */
+        "::WebkitScrollbar": {          /* Hide scrollbar for Chrome, Safari and Opera */
+            display: "none"
+        }
     },
     cardImage: {
         width: "90%",
@@ -53,7 +41,6 @@ const useStyles = makeStyles((theme) => ({
         textAlign: 'center',
 
     },
-    
 }));
 
 export default function FifteenRulesPage() {
@@ -62,9 +49,18 @@ export default function FifteenRulesPage() {
     return (
         <React.Fragment>
             <Navbar pageTitle={"The 15 Game"} />
-            <Container className={classes.scrollingContainer} maxWidth='sm' >
+            <Container className={classes.scrollingContainer} 
+                maxWidth='sm' 
+                style={{
+                    "scrollbarWidth": "none",       /* Firefox */
+                    "MsOverflowStyle": "none",      /* IE and Edge */
+                    "::WebkitScrollbar": {          /* Hide scrollbar for Chrome, Safari and Opera */
+                        display: "none"
+                    }}}
+            >
                 <Title />
-                <CardGrid />
+                <PlayModeIconGrid />
+                {/* <CardGrid /> */}
                 <Rules />
                 <TeachingTips /> 
             </Container>
@@ -74,7 +70,7 @@ export default function FifteenRulesPage() {
 
 function Title() {
     return (
-        <Typography align='center' color='textPrimary' variant='h2' gutterBottom >
+        <Typography align='center' color='textPrimary' variant='h1' gutterBottom style={{ paddingTop: "2rem" }} >
             The Fifteen Game
         </Typography>
     )
@@ -89,14 +85,14 @@ function Rules() {
             <Typography align='justify' variant='body1' color='textPrimary' gutterBottom >
                 The Fifteen Game is a two-player turn-based game.
                 It is traditionally played with a set of numbered cards but it can easily be adapted for
-                paper-and-pencil play. Players alternate claiming one of the numbered cards each turn until someone wins or no cards remain.
+                paper-and-pencil play. Each turn a player must claim one of the remaining numbered cards. 
             </Typography>
             <Typography align='left' variant="h6" color='textPrimary' gutterBottom >
                 Winning
             </Typography>
             <Typography align='justify' variant='body1' color='textPrimary' style={{ paddingBottom: "1rem" }} >
                 Both players have the same goal: be first to collect a set of exactly Three cards that adds up to exactly Fifteen.
-                You may collect more than three cards, play continues until all nine cards are claimed.
+                You may collect more than three cards, play continues until someone wins or all nine cards are claimed.
                 If you do collect more than three cards you will have to ignore any extras when looking for winning combos.
                 If all nine cards are claimed and neither player has a three-card subset that sums to 15 then the game ends a draw.
             </Typography>
@@ -110,65 +106,66 @@ function Rules() {
                 <br /> &#8226; &nbsp; Taking 2 6 7 does WIN even if you have claimed additional cards.
                 <br /> &#8226; &nbsp; Other examples of winning combos:&nbsp;&nbsp;2 5 8&nbsp;&nbsp;and&nbsp;&nbsp;1 6 8
             </Typography>
-            <Typography align='justify' variant='body1' color='textPrimary' gutterBottom >
-                Bonus Questions: How many different three card combinations are there total? How many of them sum to 15?
-            </Typography>
         </React.Fragment>
     )
 }
 
-function CardGrid() {
+function PlayModeIconGrid() {
     return (
-        <Grid container spacing={3} style={{ paddingBottom: "2rem" }}  >
+        <Grid container spacing={5} style={{ marginBottom: "2rem" }}  >
             <Grid item xs={1} />
             <Grid item xs={5} >
-                <GameCard
+                <IconCard
                     title="Play vs. Human"
-                    image={childEmoji}
                     to='/fifteen_vs_human'
+                    iconClassName="fas fa-user-friends"
                 />
+                
             </Grid>
             <Grid item xs={5} >
-                <GameCard
+                <IconCard
                     title="Play vs. Bot"
-                    image={botEmoji}
                     to='/fifteen_vs_bot'
+                    iconClassName="fas fa-robot"
                 />
             </Grid>
-            {/* <Grid item xs={4} >
-                <GameCard
-                    title="Play vs. Bot"
-                    image={botEmoji}
-                    to='/fifteen_rules'
-                />
-            </Grid> */}
         </Grid>
     )
 }
-
-function GameCard(props) {
+function IconCard(props) {
     const classes = useStyles();
-    let { title, image, to } = props
+    let { title, to, iconClassName } = props
 
     return (
-        <Card 
-            className={classes.cardRoot} 
+        <Button
+            component={RouterLink}
+            to={to}
+            fullWidth
+            style={{ 
+                border: "solid white 1px", 
+                backgroundColor: "black",
+                height: "12rem",
+            }}
         >
-            <CardHeader
-                title={title}
-                className={classes.cardTitle}
-                titleTypographyProps={{
-                    variant: "h5",
-                    align: "center"
-                }}
-            />
-            <CardMedia
-                image={image}
-                className={classes.cardImage}
-                component={RouterLink}
-                to={to}
-            />
-        </Card>
+            <Box style={{ display: "flex", flexDirection: "column", alignItems: 'center' }} >
+                <Typography
+                    children={title}
+                    variant="h4"
+                    gutterBottom
+                    align="center"
+                />
+                <Icon className={iconClassName}
+                    classes={{ root: classes.icon }}
+                    style={{ 
+                        width: "10rem",
+                        fontSize: "6rem",
+                        display: "flex", 
+                        flexDirection: "column", 
+                        alignItems: 'center'
+                    }}
+                />
+            </Box>
+        </Button>
     )
 }
 
@@ -178,78 +175,66 @@ function TeachingTips() {
             <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom style={{ paddingTop: "1.5rem"}} >
                 Teaching Tips
             </Typography>
-            <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                The Fifteen Game only takes a few minutes to play, making it an ideal brain-break activity.
-                You can also set up a bracket and have a class-wide or school-wide championship tournament.
-            </Typography>
-            <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
-                For students at the Elementary level, The Fifteen Game is a fantastic way to build automaticity at
-                mentally computing sums and differences.
-                Middle School students will learn about combinations, permutations, and symmetry as they build their tree diagrams
-                and map out all the possible strategies in Tic Tac Toe.
-                High Schoolers can learn what it means to write a rigorous proof in a familiar context.
-                They can also apply their algebra skills to create a Magic Square from scratch.
+            <Typography paragraph align='justify' component='p' variant='body1' gutterBottom >
+                The Fifteen Game has something to offer for students of all ages and at levels of mathematical training. 
+                Each round only takes takes a few minutes to play, making it an ideal for a brain-break activity during class.
+                If you want to set up a bracket and have a class-wide or school-wide championship tournament, I recommend the free tool available at <a href="https://brackethq.com/" >Bracket HQ</a>.
             </Typography>
 
-            <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom >
-                Seeing the Similarity Beneath the Surface
+            <Typography align='left' variant='h4' color='textPrimary' gutterBottom >
+                Elementary School
             </Typography>
-            <Typography paragraph align='justify' color='textPrimary' component='p' variant='body1' gutterBottom >
+            <Typography align='left' variant='h6' color='textPrimary' gutterBottom >
+                Mental Math Fluency and Thinking Out Loud
+            </Typography>
+            <Typography paragraph align='justify' component='p' variant='body1' gutterBottom >
+                For students at the Elementary level, The Fifteen Game is a fantastic way to build automaticity at mentally computing sums and differences within 20 (a 1st grade standard).
+                Even for students who have mastered basic addition, trying to formulate strategies that involve offence and defense will push them to a new depth of understanding. 
+                Make note of the pairs that add up to 10 and the fact that any of these four pairs is enough to win if you get the 5.
+                Be sure to get kids talking! Thinking out loud is a valuable life skill that games like this are excellent tools for teaching. 
+                Keep playing against the bot in "Easy" mode until you can win well over half the time.
+            </Typography>
+
+            <Typography align='left' variant='h4' color='textPrimary' gutterBottom >
+                Middle School
+            </Typography>
+            <Typography align='left' variant='h6' color='textPrimary' gutterBottom >
+                Combinations and Permutations
+            </Typography>
+            <Typography paragraph align='justify'  component='p' variant='body1'  >
+                Students at the Middle School level who have already gained a feel for the game can take there understanding to the next level by exploring questions like:
+                <br /> &#8226; &nbsp; How many different three card combinations are there total? 
+                <br /> &#8226; &nbsp; How many of those combinations sum to 15?
+                <br /> &#8226; &nbsp; What are the sums of all the combinations that don't sum to 15?
+                <br /> &#8226; &nbsp; Would this game be fun to play with a different targt number than 15?
+            </Typography>
+            <Typography paragraph align='justify' component='p' variant='body1' gutterBottom >
+                Entertaining questions like these will set kids up to learn about combinations and permutations. Once they have answered all of the above questions
+                you can inform kids that the Fifteen Game is actually Tic Tac Toe in disguise. Don't explain how though! As teachers we have to withold
+                some information to teach kids how to go get what they want with their own research!
+            </Typography>
+
+            <Typography align='left' variant='h4' color='textPrimary' gutterBottom >
+                High School: Similarity Beneath the Surface
+            </Typography>
+            <Typography align='left' variant='h6' color='textPrimary' gutterBottom >
+                Combinations and Permutations
+            </Typography>
+            <Typography paragraph align='justify' component='p' variant='body1' gutterBottom >
                 Many problems can be solved with less work if, instead of starting from scratch, you look for similarities between the problem
-                at hand and other problems that have already been solved.
-                Two real-world problems may seem unrelated at first even though they are behaving according to the same
-                underlying patterns. Being familiar with mathematics makes you better at noticing when this is the case!
-                Experience this for yourself by playing the two games featured here.
-                Though they feel nothing alike when you first play them, the Fifteen Game and Tic-Tac-Toe are actually the same game.
-                In fact, the bot in this app uses the same exact code to select its moves when playing Tic Tac Toe and the Fifteen Game.
-                The only thing that is different is how these moves are represented visually.
-                If you have mastered Tic-Tac-Toe but still find yourself losing to my bot at the Fifteen Game then I suggest looking into "Magic Squares".
+                at hand and other problems that you already know how to solve. Have you <em>mastered</em> Tic Tac Toe to the point where you 
+                never lose and also never miss a chance to win when you opponent slips up?  Does the bot still get the best of you at the 15 game 
+                occasionally? Well you know by now that these two games are the same, make yourself a visual aide!
+            </Typography>
+            <Typography paragraph align='justify' component='p' variant='body1' gutterBottom >
+                Magic Squares are an ancient mathematical curiosity. They can be made in many sizes but here is what you will be trying to create:
+                <br /> &#8226; &nbsp; A three by three grid that has each of the numbers 1 through 9 in it one time each.
+                <br /> &#8226; &nbsp; The three digits in each row, column, and diagonal must add up to 15.
+                <br /> Start listing combos and put your algebra and logical deduction skills to work! What can you proove as you go?
+                There is more than one way to complete this task but ask yourself things like:
+                <br /> &#8226; &nbsp; What is special about the center cell? Which numbers can go there?
+                <br /> &#8226; &nbsp; What do all the numbers that go in corners have in common?
             </Typography>
         </React.Fragment>
     )
 }
-
-
-// <Typography align='left' variant='h6' color='textPrimary' gutterBottom >
-//                 Tic-Tac-Toe for Elementary Grades
-//             </Typography>
-//             <Typography variant='body1'  gutterBottom >
-//                 Kids as young as 1st grade have long enjoyed this classic paper and pencil game. 
-//                 Most children get pretty good at the game by the time they have played a few dozen rounds.
-//                 At this point I like to challenge them to find (and write down) a step by step strategy that will guarantee them a win as long as they get to go first. 
-//                 Leave students to their own devices for a while to invent ways of writing down their moves and organizing their thoughts.
-//                 Once they have had plenty of opportunity for creative struggle you may introduce them to tree diagrams and to systems of notation that assign a name to each cell.
-//             </Typography>
-//             <Typography align='left' variant='h6' color='textPrimary' gutterBottom >
-//                 The 15 Game for Elementary Grades
-//             </Typography>
-//             <Typography variant='body1' gutterBottom >
-//                 Kids as young as 1st grade have long enjoyed this classic paper and pencil game.
-//                 Most children get pretty good at the game by the time they have played a few dozen rounds.
-//                 At this point I like to challenge them to find (and write down) a step by step strategy that will guarantee them a win as long as they get to go first.
-//                 Leave students to their own devices for a while to invent ways of writing down their moves and organizing their thoughts.
-//                 Once they have had plenty of opportunity for creative struggle you may introduce them to tree diagrams and to systems of notation that assign a name to each cell.
-//             </Typography>
-
-
-//             <Typography align='left' variant='h6' color='textPrimary' gutterBottom >
-//                 Tic-Tac-Toe as an Intro to Proof
-//             </Typography>
-//             <Typography variant='body1'  gutterBottom >
-//                 By the time they have played a few dozen rounds of Tic-Tac-Toe, most children have gotten pretty good at the game.
-//                 At this point I like to challenge them to find (and write down) a strategy that will guarantee them a win
-//                 every time, as long as they get to make the first move. First let students invent their own ways of writing down move orders and organizing their thoughts.
-//                 Once they have had a chance to get creative and to struggle you may introduce them to tree diagrams and to systems of notation assign a letter name to each cell.
-//             </Typography>
-//             <Typography variant='body1'  gutterBottom >
-//                 Generally, students will come up with a few different strategies that they think guarantee a win, but one by one
-//                 counterexamples will be found and most kids will eventually intuit that this is an impossible task.
-//                 At this point I modify the objective: If you think there is no strategy that guarantees a win, then prove that instead!
-//                 Prove that you can at get a draw in the worst case no matter what moves your opponent makes when they go first.
-//             </Typography>
-//             <Typography variant='body1'  gutterBottom >
-//                 The point here is that we will not settle for a final answer that is just based on intuition.
-//                 Tic Tac Toe is a classic game with a reputation for being one of the simplest strategy games, but this reputation causes many folks to
-//                 underestimate its depth. It is very common for people to asume that they have fully mastered Tic-Tac-Toe just because they know a way to never lose,
-//                 but a true master plays both offens and defense. Just because you can't force your opponent to lose doesn't mean you should let your guard down: unforced errors happen all the time!
-//             </Typography>
