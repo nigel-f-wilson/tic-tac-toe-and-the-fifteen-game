dfd860e2c3bd6507aa2a09095a0d1b928edce3cc
nolastemgarden <nolastemgarden@gmail.com>
2022-01-09 14:08:13 -0600

    Copy format from 15 game, update buttons

--- src/pages/TicTacToeRules.js
+++ src/pages/TicTacToeRules.js
@@ -8,28 +8,253 @@ import {
 import Navbar from "../components/Navbar/Navbar"
 
 // MUI  components
-import { Box, Container, Grid, Button, Typography } from '@material-ui/core'
+import {
+    Box,
+    Container,
+    Grid,
+    Button,
+    Typography,
+} from '@material-ui/core'
+import Icon from '@material-ui/core/Icon';
 
 // Custom Styling
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        minHeight: 'calc(100vh - 4rem)',
+    scrollingContainer: {
         overflowY: "scroll",
-        display: 'flex',
-        flexDirection: 'column',
-        color: theme.palette.common.white,
-        padding: '1.0rem',
+        "scrollbarWidth": "none",       /* Firefox */
+        "MsOverflowStyle": "none",      /* IE and Edge */
+        "::WebkitScrollbar": {          /* Hide scrollbar for Chrome, Safari and Opera */
+            display: "none"
+        }
     },
-    button: {
+    cardImage: {
+        width: "90%",
+        paddingTop: "86%",
+        margin: "0 0 1rem",
+        borderRadius: "0.3rem"
+    },
+    cardButton: {
+        width: "75%",
+        margin: "0 0 1rem",
         backgroundColor: theme.palette.primary.main,
         textAlign: 'center',
-        width: '95%',
+
     },
 }));
 
-export default function FifteenRulesPage() {
+export default function TicTacToeRulesPage() {
+    const classes = useStyles();
+
+    return (
+        <React.Fragment>
+            <Navbar pageTitle={"Tic Tac Toe"} />
+            <Container className={classes.scrollingContainer}
+                maxWidth='sm'
+                style={{
+                    "scrollbarWidth": "none",       /* Firefox */
+                    "MsOverflowStyle": "none",      /* IE and Edge */
+                    "::WebkitScrollbar": {          /* Hide scrollbar for Chrome, Safari and Opera */
+                        display: "none"
+                    }
+                }}
+            >
+                <Title />
+                <PlayModeIconGrid />
+                <Rules />
+                <TeachingTips />
+            </Container>
+        </React.Fragment>
+    );
+}
+
+function Title() {
+    return (
+        <Typography align='center' color='textPrimary' variant='h1' gutterBottom style={{ paddingTop: "2rem" }} >
+            Tic Tac Toe
+        </Typography>
+    )
+}
+
+function Rules() {
+    return (
+        <React.Fragment>
+            <Typography align='left' variant="h4" color='textPrimary' gutterBottom >
+                Rules of Play
+            </Typography>
+            <Typography align='justify' variant='body1' color='textPrimary' gutterBottom >
+                Tic-Tac-Toe is most often played as a paper and pencil game. 
+                Two players, one using X's and the other using O's, take turns marking one cell in a three-by-three grid.
+                Both players seek to get a three-in-a-row, to have their mark in all three cells in a any row, column,
+                or diagonal. If the whole grid is filled and neither player has managed to get a three-in-a-row then the game
+                ends a draw.
+            </Typography>
+            
+        </React.Fragment>
+    )
+}
+
+function PlayModeIconGrid() {
+    return (
+        <Grid container spacing={5} style={{ marginBottom: "2rem" }}  >
+            <Grid item xs={4} >
+                <IconCard
+                    title="Play vs. Human"
+                    to='/tic_tac_toe_vs_human'
+                    iconClassName="fas fa-user-friends"
+                />
+            </Grid>
+            <Grid item xs={4} >
+                <IconCard
+                    title="Play vs. Bot"
+                    to='/tic_tac_toe_vs_bot'
+                    iconClassName="fas fa-robot"
+                />
+            </Grid>
+            <Grid item xs={4} >
+                <IconCard
+                    title="Play with Coach"
+                    to='/tic_tac_toe_with_coach'
+                    iconClassName="fas fa-graduation-cap"
+                />
+            </Grid>
+        </Grid>
+    )
+}
+function IconCard(props) {
+    const classes = useStyles();
+    let { title, to, iconClassName } = props
+
+    return (
+        <Button
+            component={RouterLink}
+            to={to}
+            fullWidth
+            style={{
+                border: "solid white 1px",
+                backgroundColor: "black",
+                height: "12rem",
+            }}
+        >
+            <Box style={{ display: "flex", flexDirection: "column", alignItems: 'center' }} >
+                <Typography
+                    children={title}
+                    variant="h5"
+                    gutterBottom
+                    align="center"
+                />
+                <Icon className={iconClassName}
+                    classes={{ root: classes.icon }}
+                    style={{
+                        width: "10rem",
+                        fontSize: "6rem",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: 'center'
+                    }}
+                />
+            </Box>
+        </Button>
+    )
+}
+
+function TeachingTips() {
+    return (
+        <React.Fragment>
+            <Typography align='left' color='textPrimary' component='h4' variant='h4' gutterBottom style={{ paddingTop: "1.5rem" }} >
+                Teaching Tips
+            </Typography>
+            <Typography paragraph align='justify' component='p' variant='body1' gutterBottom >
+                The Fifteen Game has something to offer for students of all ages and at levels of mathematical training.
+                Each round only takes takes a few minutes to play, making it an ideal for a brain-break activity during class.
+                If you want to set up a bracket and have a class-wide or school-wide championship tournament, I recommend the free tool available at <a href="https://brackethq.com/" >Bracket HQ</a>.
+            </Typography>
+
+            <Typography align='left' variant='h4' color='textPrimary' gutterBottom >
+                Elementary School
+            </Typography>
+            <Typography align='left' variant='h6' color='textPrimary' gutterBottom >
+                Mental Math Fluency and Thinking Out Loud
+            </Typography>
+            <Typography paragraph align='justify' component='p' variant='body1' gutterBottom >
+                For students at the Elementary level, The Fifteen Game is a fantastic way to build automaticity at mentally computing sums and differences within 20 (a 1st grade standard).
+                Even for students who have mastered basic addition, trying to formulate strategies that involve offence and defense will push them to a new depth of understanding.
+                Make note of the pairs that add up to 10 and the fact that any of these four pairs is enough to win if you get the 5.
+                Be sure to get kids talking! Thinking out loud is a valuable life skill that games like this are excellent tools for teaching.
+                Keep playing against the bot in "Easy" mode until you can win well over half the time.
+            </Typography>
+
+            <Typography align='left' variant='h4' color='textPrimary' gutterBottom >
+                Middle School
+            </Typography>
+            <Typography align='left' variant='h6' color='textPrimary' gutterBottom >
+                Combinations and Permutations
+            </Typography>
+            <Typography paragraph align='justify' component='p' variant='body1'  >
+                Students at the Middle School level who have already gained a feel for the game can take there understanding to the next level by exploring questions like:
+                <br /> &#8226; &nbsp; How many different three card combinations are there total?
+                <br /> &#8226; &nbsp; How many of those combinations sum to 15?
+                <br /> &#8226; &nbsp; What are the sums of all the combinations that don't sum to 15?
+                <br /> &#8226; &nbsp; Would this game be fun to play with a different targt number than 15?
+            </Typography>
+            <Typography paragraph align='justify' component='p' variant='body1' gutterBottom >
+                Entertaining questions like these will set kids up to learn about combinations and permutations. Once they have answered all of the above questions
+                you can inform kids that the Fifteen Game is actually Tic Tac Toe in disguise. Don't explain how though! As teachers we have to withold
+                some information to teach kids how to go get what they want with their own research!
+            </Typography>
+
+            <Typography align='left' variant='h4' color='textPrimary' gutterBottom >
+                High School: Similarity Beneath the Surface
+            </Typography>
+            <Typography align='left' variant='h6' color='textPrimary' gutterBottom >
+                Combinations and Permutations
+            </Typography>
+            <Typography paragraph align='justify' component='p' variant='body1' gutterBottom >
+                Many problems can be solved with less work if, instead of starting from scratch, you look for similarities between the problem
+                at hand and other problems that you already know how to solve. Have you <em>mastered</em> Tic Tac Toe to the point where you
+                never lose and also never miss a chance to win when you opponent slips up?  Does the bot still get the best of you at the 15 game
+                occasionally? Well you know by now that these two games are the same, make yourself a visual aide!
+            </Typography>
+            <Typography paragraph align='justify' component='p' variant='body1' gutterBottom >
+                Magic Squares are an ancient mathematical curiosity. They can be made in many sizes but here is what you will be trying to create:
+                <br /> &#8226; &nbsp; A three by three grid that has each of the numbers 1 through 9 in it one time each.
+                <br /> &#8226; &nbsp; The three digits in each row, column, and diagonal must add up to 15.
+                <br /> Start listing combos and put your algebra and logical deduction skills to work! What can you proove as you go?
+                There is more than one way to complete this task but ask yourself things like:
+                <br /> &#8226; &nbsp; What is special about the center cell? Which numbers can go there?
+                <br /> &#8226; &nbsp; What do all the numbers that go in corners have in common?
+            </Typography>
+        </React.Fragment>
+    )
+}
+
+
+
+
+
+
+
+
+
+// import { makeStyles } from '@material-ui/core/styles';
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         width: '100%',
+//         minHeight: 'calc(100vh - 4rem)',
+//         overflowY: "scroll",
+//         display: 'flex',
+//         flexDirection: 'column',
+//         color: theme.palette.common.white,
+//         padding: '1.0rem',
+//     },
+//     button: {
+//         backgroundColor: theme.palette.primary.main,
+//         textAlign: 'center',
+//         width: '95%',
+//     },
+// }));
+
+function FifteenRulesPage() {
     const classes = useStyles();
 
     return (
